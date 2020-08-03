dc64e13fd87a5da41928578322eecf992da8f439
harogumi <haonguyen.thecixteam@gmail.com>
2020-08-04 01:23:35 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Fragment, useState } from 'react';
+import './App.css';
+import Paginate from './Paginate';
+import ReactPaginate from 'react-paginate';
+
+function App() {
+  const [page, setPage] = useState(1);
+
+  return (
+    <Fragment>
+      <Paginate
+        curPage={page}
+        totalRec={300}
+        recPerPage={20}
+        maxPageDisplay={10}
+        onChangePage={(page) => setPage(page)}
+      />
+      {/* <ReactPaginate
+        initialPage={0}
+        pageCount={15}
+        pageRangeDisplayed={10}
+        marginPagesDisplayed={1}
+        onPageChange={(page) =>  setPage(page)}
+        pageClassName="btn btn-light"
+        activeClassName="btn btn-danger"
+        activeLinkClassName="link-active"
+        previousClassName="btn btn-light"
+        nextClassName="btn btn-light"
+      /> */}
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/Paginate/index.jsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import "./style.css";
+
+const Pagination = (props) => {
+  const { totalRec, curPage = 1, recPerPage = 10, maxPageDisplay = 10, onChangePage } = props;
+
+  const [state, setState] = useState({});
+
+  useEffect(() => {
+    handlePage();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [curPage]);
+
+  const handlePage = () => {
+    const totalPages = Math.ceil(totalRec / recPerPage);
+
+    let startPage;
+    let endPage;
+
+    if (totalPages <= maxPageDisplay) {
+      startPage = 1;
+      endPage = totalPages;
+    } else {
+
+      if (curPage < Math.ceil(maxPageDisplay / 2) + 1) {
+        startPage = 1;
+        endPage = maxPageDisplay;
+      } 
+      else if (curPage + (maxPageDisplay - (Math.ceil(maxPageDisplay / 2) + 1)) >= totalPages) {
+        startPage = totalPages - (maxPageDisplay - 1);
+        endPage = totalPages;
+      } 
+      else {
+        startPage = curPage - Math.floor(maxPageDisplay / 2);
+        endPage = curPage + Math.ceil(maxPageDisplay / 2) - 1;
+      }
+
+    }
+
+    const startIndex = (curPage - 1) * recPerPage;
+    const endIndex = Math.min(startIndex + recPerPage - 1, totalRec - 1);
+
+    const pages = [...Array(endPage + 1 - startPage).keys()].map(i => startPage + i);
+
+    if (curPage < 1 || curPage > Math.ceil(totalRec / recPerPage)) {
+      return;
+    }
+
+    setState({ curPage, startPage, endPage, startIndex, endIndex, pages });
+  };
+
+  const generatePages = state.pages && state.pages.map((page) => {
+    return (
+      <div key={page}>
+        <button
+          onClick={() => onChangePage(page)}
+          className={`btn ${curPage === page ? 'btn-danger' : 'btn-light'}`}
+        >
+          {page}
+        </button>
+      </div>
+    )
+  });
+
+  const jumpMinStep = () => onChangePage(1);
+  const jumpMaxStep = () => onChangePage(Math.ceil(totalRec / recPerPage));
+
+  const decStep = () => {
+    if(curPage > 1 && curPage <= Math.ceil(totalRec / recPerPage)) {
+      onChangePage(curPage - 1);
+    }
+  };
+
+  const incStep = () => {
+    if(curPage >= 1 && curPage < Math.ceil(totalRec / recPerPage)) {
+      onChangePage(curPage + 1);
+    }
+  };
+
+  return (
+    <div className="__pagination">
+      <div className="d-flex">
+        <button className="btn btn-light" onClick={jumpMinStep}>{"<<"}</button>
+        <button className="btn btn-light" onClick={decStep}>{"<"}</button>
+          {generatePages}
+        <button className="btn btn-light" onClick={incStep}>{">"}</button>
+        <button className="btn btn-light" onClick={jumpMaxStep}>{">>"}</button>
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
